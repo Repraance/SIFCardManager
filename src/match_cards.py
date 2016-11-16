be749019d9853424ec15a11e19fbcd716408baa6
Repraance <reprance@gmail.com>
2016-11-16 17:35:15 +0800

    rarity and attribute detection done

--- src/match_cards.py
+++ src/match_cards.py
@@ -1,10 +1,11 @@
 # coding:utf-8
 
+import os
 import json
-import cv2
-import sys
 import tkFileDialog
+import cv2
 import numpy as np
+from collections import defaultdict
 
 
 class Matcher:
@@ -13,8 +14,12 @@ class Matcher:
         self.raw_screen_shot = list()
         self.card_icon = list()
         self.card_type = list()
-        self.type_index = dict()
+        self.card_rarity = list()
+        self.card_attr = list()
+        self.type_index = defaultdict(list)
         self.attr_index = ['cool', 'pure', 'smile']
+        self.rarity_index = ['N', 'R', 'SR', 'SSR', 'UR']
+        self.rarity_symbol_pixel_count_index = dict.fromkeys(self.rarity_index)
 
     # 使用tk打开截图图像文件
     def input_screen_shot(self):
@@ -68,11 +73,13 @@ class Matcher:
 
     # 获取当前卡截图与模板图像的最小差值
     def get_min_diff(self, src, template, global_offset=(0, 0)):
-        diff_mask = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        diff_mask = template
+        if template.ndim == 3:
+            diff_mask = cv2.cvtColor(diff_mask, cv2.COLOR_BGR2GRAY)
         offset = [0, 0]
         pixel_sum_min = 100000000
-        for offset_i in range(0, 4):
-            for offset_j in range(0, 4):
+        for offset_i in range(0, 3):
+            for offset_j in range(0, 3):
                 cropped = src[offset_j + global_offset[0]: offset_j + global_offset[0] + template.shape[0],
                               offset_i + global_offset[1]: offset_i + global_offset[1] + template.shape[1]]
                 diff = cv2.subtract(template, cropped, mask=diff_mask)
@@ -88,32 +95,23 @@ class Matcher:
         return pixel_sum_min
 
     # 创建属性和稀有度的类型索引
-    def generate_type_index(self, card_data_json):
+    def generate_type_index(self, card_data_json='../data/card_data.json'):
         card_data = json.load(file(card_data_json))
         for card_id in card_data:
             # 检测是否含有键'rarity'和'attribute'
             if 'rarity' and 'attribute' in card_data[card_id]:
                 type_name = card_data[card_id]['rarity'] + card_data[card_id]['attribute']
+                self.type_index[type_name].append(card_id)
 
-                # 如果没有当前类型的键 将该键创建为dict
-                if type_name not in self.type_index:
-                    self.type_index[type_name] = dict()
-                current_type = self.type_index[type_name]
+    # 因为稀有度标志图像大小不一 需要为其创建像素数索引
+    def generate_rarity_symbol_pixel_count_index(self):
+        for rarity_name in self.rarity_symbol_pixel_count_index:
+            img = cv2.imread('../assets/symbol/' + rarity_name + '.png', 0)
+            if img is not None:
+                cropped = img[0: 37, 0: 40]
+                self.rarity_symbol_pixel_count_index[rarity_name] = self.pixel_count(cropped)
 
-                # 如果没有'card'键 在type_index[type_name]中将其创建为list
-                if 'cards' not in current_type:
-                    current_type['cards'] = list()
-                # 将当前卡id添加入list
-                current_type['cards'].append(card_id)
-
-                # 添加'pixel_count'键并赋值，只在第一次执行
-                if 'pixel_count' not in current_type:
-                    img = cv2.imread('common/' + type_name + '.png', 0)
-                    if img is not None:
-                        cropped = img[0: 37, 0: 25]
-                        current_type['pixel_count'] = self.pixel_count(cropped)
-
-    # 将截图分成单独的卡，存储在card_icon中
+    # 将截图分成单独的卡，存储在 card_icon 中
     def split(self):
         card_list = self.remove_edge(self.raw_screen_shot[0])
         col_start = [3, 137, 272, 407, 541, 676, 811, 945]
@@ -123,6 +121,10 @@ class Matcher:
                 single_card = card_list[row_start[j]: row_start[j] + self.card_width,
                                         col_start[i]: col_start[i] + self.card_width]
                 self.card_icon.append(single_card)
+
+    def prepare_data(self):
+        self.generate_type_index()
+        self.generate_rarity_symbol_pixel_count_index()
 
     @staticmethod
     def get_max_value_index(l):
@@ -134,6 +136,7 @@ class Matcher:
                 max_value_index = i
         return max_value_index
 
+    # 检测卡的属性（颜色）
     def detect_attr(self):
         for card in self.card_icon:
             color = list()
@@ -144,46 +147,44 @@ class Matcher:
             for i in range(0, 3):
                 color_count.append(color.count(i))
             attr = self.attr_index[self.get_max_value_index(color_count)]
-            print attr
+            self.card_attr.append(attr)
 
+    # 检测卡的稀有度
     def detect_rarity(self):
-        card = self.card_icon[1]
-        card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
-        card_cropped = card_gray[1:129, 1:129]
-        rarity = ['N', 'R', 'SR', 'SSR', 'UR']
-        for rr in rarity:
-            tem = cv2.imread('common/tem/' + rr + 'all.png', 0)
-            diff = cv2.subtract(tem, card_cropped, mask=tem)
-            print rr, self.pixel_sum(diff)
-            cv2.imshow('card', card_cropped)
-            cv2.imshow('rr', tem)
-            cv2.imshow('diff', diff)
-            cv2.waitKey()
-
-    def detect_type(self):
         for card in self.card_icon:
+            card_gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
             min_average_pixel_sum = 100000000
-            result_type_name = ''
-            for type_name in self.type_index:
-                template_raw = cv2.imread('common/' + type_name + '.png')
+            result_rarity_name = ''
+            for rarity_name in self.rarity_index:
+                template_raw = cv2.imread('../assets/symbol/' + rarity_name + '.png', 0)
                 if template_raw is not None:
-                    template = template_raw[0: 37, 0: 25]
-                    pixel_count = float(self.type_index[type_name]['pixel_count'])
-                    current_average_pixel_sum = self.get_min_diff(card, template) / pixel_count
+                    template = template_raw[0: 40, 0: 43]
+                    # pixel_count = float(self.rarity_symbol_pixel_count_index[rarity_name])
+                    pixel_count = 1
+                    if rarity_name == 'N':
+                        pixel_count = 0.5
+                    current_average_pixel_sum = self.get_min_diff(card_gray, template) / pixel_count
                     # print type_name, current_average_pixel_sum
                     if current_average_pixel_sum < min_average_pixel_sum:
                         min_average_pixel_sum = current_average_pixel_sum
-                        result_type_name = type_name
-            self.card_type.append(result_type_name)
-            print result_type_name
+                        result_rarity_name = rarity_name
+            self.card_rarity.append(result_rarity_name)
+
+    def detect_type(self):
+        self.detect_rarity()
+        self.detect_attr()
+        card_count = len(self.card_icon)
+        for i in range(0, card_count):
+            self.card_type.append(self.card_rarity[i] + self.card_attr[i])
 
     def match(self):
         face_mask = np.zeros((128, 128, 1), np.uint8)
         cv2.circle(face_mask, (63, 63), 53, 255, -1)
 
 if __name__ == '__main__':
+    os.chdir('../test')
     matcher = Matcher()
+    matcher.prepare_data()
     matcher.input_screen_shot()
     matcher.split()
-    matcher.generate_type_index('/data/card_data.json')
     matcher.detect_type()
--- src/test.py
+++ src/test.py
@@ -3,12 +3,14 @@
 from cv2 import *
 import matplotlib.pyplot as plt
 
-n = imread('common/tem/SRall.png', 0)
-rectangle(n, (6, 9), (27, 26), 0, -1)
-rectangle(n, (0, 0), (2, 36), 0, -1)
-rectangle(n, (0, 0), (33, 4), 0, -1)
-rectangle(n, (30, 0), (33, 33), 0, -1)
-rectangle(n, (0, 32), (32, 36), 0, -1)
-imwrite('11.png', n)
+n = imread('../assets/symbol/N.png', 0)
+r = imread('../assets/symbol/R.png', 0)
+sr = imread('../assets/symbol/SR.png', 0)
+ssr = imread('../assets/symbol/SSR.png', 0)
+ur = imread('../assets/symbol/UR.png', 0)
 imshow('n', n)
+imshow('r', r)
+imshow('sr', sr)
+imshow('ssr', ssr)
+imshow('ur', ur)
 waitKey()
